549c0f800d511ab56154b747b06fb74985ac145e
Ziad Mugraby <ziad@itxi.net>
2014-11-12 14:39:00 +0200

    Updated documentation on constants.h

--- BeaconRegistry/Constants.h
+++ BeaconRegistry/Constants.h
@@ -8,13 +8,13 @@
 
 
 //API INFO
-#define BeaconRegistryAPIKey   @"YOUR_API_KEY"
-#define AppId   @"YOUR_APP_ID"
+#define BeaconRegistryAPIKey   @"YOUR_API_KEY"  // Obtain this from developer.aero
+#define AppId   @"YOUR_APP_ID"     // Obtain this from developer.aero
 #define EndPointURL  @"https://cube.api.aero"
 #define Timeout 15
 
 //Flight INFO
-#define AirportCode @"YOUR_AIRPORT_TCODE"
+#define AirportCode @"YOUR_AIRPORT_CODE"  //IATA Three letter code like DFW or JFK
 #define Flight   @"XS0001"
 #define FlightDate   @"2014-11-11"
 #define PassengerName   @"Bilal Itani"
